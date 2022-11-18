0aa8cf1c3d4312854593116a620d824baec59438
Shahad <shahad.sidek@gmail.com>
2022-11-18 17:37:42 +0300

    optimization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters:[],
+      searchField:''
+    };
+    console.log("Constructor Method - Value of monsters", this.state.monsters);
+    console.log("Constructor Method - Value of searchField", this.state.searchField);
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=> response.json())
+    .then((users)=>this.setState (
+      ()=>{
+        return{monsters: users}
+      },
+      ()=>{
+        console.log("didMount Method - Value of monsters", this.state.monsters);
+        console.log("didMount Method - Value of searchField", this.state.searchField);
+      }
+    ));
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(
+      () => {
+        return{ searchField };
+      }
+    );
+  }
+  
+  render(){
+    
+    const { monsters, searchField} = this.state;
+    const { onSearchChange } = this; 
+    const filteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
+    console.log("Render Method - Value of monsters", monsters)
+    console.log("Render Method - Value of searchField",searchField)
+    
+    return(
+      <div className="App">
+        <input 
+          className="search-box"
+          type="search"
+          placeholder='Search Monsters'
+          onChange={onSearchChange}
+        />
+        { 
+          filteredMonsters.map((monster) => {
+            return <h1 key={monster.id}>{monster.name}</h1>
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
